b1202277be5c3db5765d8b7a16430a2d7306a2b5
Isabel Pijnenburg <433195@student.fontys.nl>
2021-10-06 00:45:37 +0200

    Choose path

--- Assets/Scripts/RouteMovement.cs
+++ Assets/Scripts/RouteMovement.cs
@@ -5,10 +5,14 @@ using UnityEngine;
 public class RouteMovement : MonoBehaviour
 {
     public GameObject Arrows;
+    public PlayerMovement playerMovement;
+    public Transform[] leftRouteWaypoints;
+    public Transform[] rightRouteWaypoints;
     public bool currentWayPointIsRoute;
 
     void Start()
     {
+        //playerMovement = GetComponent<PlayerMovement>();
         currentWayPointIsRoute = false;
         Arrows.SetActive(false);
     }
@@ -17,11 +21,31 @@ public class RouteMovement : MonoBehaviour
     {
         if (other.gameObject.CompareTag("Player"))
         {
-            
+            playerMovement.Speed = 0;
             currentWayPointIsRoute = true;
             Debug.Log(currentWayPointIsRoute);
             Debug.Log("triggered");
             Arrows.SetActive(true);
+        }
+    }
+
+    public void TakeLeftRoute()
+    {
+        if (currentWayPointIsRoute)
+        {
+            Arrows.SetActive(false);
+            Debug.Log("Took the left route");
+            playerMovement.Speed = 5; // Temporary solution!
+        }
+    }
+
+    public void TakeRightRoute()
+    {
+        if (currentWayPointIsRoute)
+        {
+            Arrows.SetActive(false);
+            Debug.Log("Took the right route");
+            playerMovement.Speed = 5; // Temporary solution!
         }
     }
 }
